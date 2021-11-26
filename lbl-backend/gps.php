43befdf6ce2ada5e1004a982545b4bbb3920acae
alexandre bisetti <alexandre.bisetti@etu.unige.ch>
2021-11-26 16:00:02 +0100

    add stuffs

--- lbl-backend/gps.php
+++ lbl-backend/gps.php
@@ -4,13 +4,26 @@ require_once  __DIR__ . "/src/pdo.php";
 
 switch ($requestMethod) {
     case 'GET':
-        $stmt = $pdo->prepare("SELECT * FROM espaces WHERE idBatiment = :id");
-        $stmt->execute(array(":id" => $_GET['id']));
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($row === false) {
-            echo (json_encode(array("Error DB" => "Error query")));
+        if (isset($_GET['batiment']) && isset($_GET['cote']) && isset($_GET['secteur'])) {
+            $cote = explode(" ", $_GET['cote']);
+            //echo (json_encode(array("Error DB" =>  $cote[0])));
+            $stmt = $pdo->prepare(
+                "SELECT locator.longitude, locator.latitude, locator.estAccessible, espaces.nomLong, espaces.longitude, espaces.latitude, espaces.p1Latitude, espaces.p1Longitude, espaces.p2Latitude, espaces.p2Longitude, espaces.p3Latitude, espaces.p3Longitude, espacesimages.etage, espacesimages.url  FROM espacesimages, espaces, locator WHERE espaces.idBatiment = locator.idBatiment AND espacesimages.idBatiment = espaces.idBatiment AND espaces.nomCourt = :bat AND locator.secteur = :sect AND locator.racineDeweyDebut = :racine_debut AND locator.racineDeweyFin = :racine_fin"
+            );
+            $stmt->execute(array(
+                ":bat" => $_GET['batiment'],
+                ":sect" => $_GET['secteur'],
+                ":racine_debut" => $cote[0],
+                ":racine_fin" => $cote[0]
+            ));
+            $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if ($row === false) {
+                echo (json_encode(array("Error DB" => "Error query")));
+            } else {
+                echo (json_encode(array("message" => $row)));
+            }
         } else {
-            echo (json_encode(array("message" => $row)));
+            echo (json_encode(array("Error" => "Missing parameters")));
         }
         break;
     default:
